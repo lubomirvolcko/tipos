ad723efa417ce68b183676eebd03cfea16da7cee
lubomirvolcko <lubomir.volcko@akademiasovy.sk>
2018-03-27 12:36:55 +0200

    insert text to error label

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,27 @@
 package sample;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    public Button btnLogin;
+    public Label lblError;
+    public PasswordField txtPassword;
+    public TextField txtLogin;
+
+    public void clickLogin(){
+        String login = txtLogin.getText().trim();
+        String password = txtPassword.getText().trim();
+        System.out.println(login+" "+password);
+        if (login.length()<1 || password.length()<1){
+            lblError.setText("Wrong login or password!");
+        }else {
+
+        }
+
+    }
+
 }
